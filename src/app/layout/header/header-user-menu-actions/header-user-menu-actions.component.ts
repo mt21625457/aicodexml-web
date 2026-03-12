a351276b41d75bdf08b8163ac0041148ce7d2dc4
yangjianbo <yangjianbo@leagsoft.com>
2026-03-12 16:54:50 +0800

    feat(i18n): add header language switcher

--- src/app/layout/header/header-user-menu-actions/header-user-menu-actions.component.ts
+++ src/app/layout/header/header-user-menu-actions/header-user-menu-actions.component.ts
@@ -1,9 +1,21 @@
-import {Component} from '@angular/core';
+import {ChangeDetectionStrategy, Component, inject} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenuItem} from '@angular/material/menu';
+import {TranslatePipe} from '@ngx-translate/core';
+import {LocaleService} from '~/shared/services/locale.service';
 
 @Component({
     selector: 'sm-header-user-menu-actions',
     templateUrl: './header-user-menu-actions.component.html',
     styleUrls: ['./header-user-menu-actions.component.scss'],
+    imports: [MatMenuItem, MatIconModule, TranslatePipe],
+    changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderUserMenuActionsComponent {
+  protected localeService = inject(LocaleService);
+  protected currentLanguage = this.localeService.currentLanguage;
+
+  switchLanguage(language: 'en' | 'zh-CN') {
+    this.localeService.setLanguage(language).subscribe();
+  }
 }
